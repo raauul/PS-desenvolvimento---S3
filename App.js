eae56d195d214c7db55c53d5c8060e0045a6a828
raauul <42390544+raauul@users.noreply.github.com>
2020-06-19 14:36:07 -0300

    src

---
+++ App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import api from './api';
+
+class App extends Component{
+ 
+  state={
+    filmes:[]
+  }
+ 
+  render(){
+    return(
+      <div>
+       <h2>Star wars</h2>
+      </div>
+    );
+  };
+};
+
+
+export default App;
